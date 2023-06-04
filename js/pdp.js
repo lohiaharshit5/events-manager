8b5255f96ba3fc36c08f938f1f9be5a15d015296
Harshit <harshit@Harshits-MacBook-Air.local>
2023-06-04 17:51:36 +0530

    my third commit with pdp screen

--- js/pdp.js
+++ js/pdp.js
@@ -1,29 +1,48 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
-function plusSlides(n){
-    showSlides(slideIndex+=n);
-
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function currentSlide(n){
-    showSlides(slideIndex=n);
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-function showSlides(n){
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if(n>slides.length){slideIndex = 1}
-    if(n<1){slideIndex = slides.length}
-    for(i=0; i<slides.length;i++){
-        slides[i].style.display = "none";
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("my-slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
 
-    }
-    for(i=0; i<dots.length;i++){
-        dots[i].className = dots[i].className.replace("active", "");
-        }
-        slides[slideIndex-1].style.display="block";
-        dots[slideIndex-1].className += "active";
-    
+// Auto slide
+var autoSlideIndex = 0;
+autoShowSlides();
+
+function autoShowSlides() {
+  var i;
+  var slides = document.getElementsByClassName("my-slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  autoSlideIndex++;
+  if (autoSlideIndex > slides.length) {
+    autoSlideIndex = 1;
+  }
+  slides[autoSlideIndex - 1].style.display = "block";
+  setTimeout(autoShowSlides, 5000); // Change image every 2 seconds
 }
