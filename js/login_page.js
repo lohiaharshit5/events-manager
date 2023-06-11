7578df3127414702218fe3579fa107d7bc7420b4
Harshit <harshit@Harshits-MacBook-Air.local>
2023-06-11 19:29:25 +0530

    login page changes

---
+++ js/login_page.js
@@ -0,0 +1,7 @@
+var otp = document.getElementById("otp");
+var sendotpButton = document.getElementById("send-otp");
+
+sendotpButton.addEventListener("click", ()=>{
+    otp.classList.toggle("hide")
+;}
+);
