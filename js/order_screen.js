56c35dfafdff9a709c43b0907757305778274ca2
Harshit <harshit@Harshits-MacBook-Air.local>
2023-07-31 16:04:20 +0530

    some changes storagedata

--- js/order_screen.js
+++ js/order_screen.js
@@ -8,16 +8,20 @@ console.log('checking');
 getDATA();
 function getDATA(){
   const user_id = localStorage.getItem("user_id");
+  const gender = localStorage.getItem("gender");
+
   console.log('in data');
 // Do something with the user_id
   console.log("User ID:", user_id);
-  const userDataString = localStorage.getItem("user_data");
+  console.log("gender:", gender);
+
+
 
 // Parse the JSON string back to an object
-  const userData = JSON.parse(userDataString);
+
 
 // Now you can access the properties of the JSON object
-  console.log("user_data:", userData);
+
   // console.log("Name:", userData.name);
   // console.log("Email:", userData.email);
 
