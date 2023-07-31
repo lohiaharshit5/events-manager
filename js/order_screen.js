5b98f386f84c9cac2560ca9c1d7d566d00de5ca9
Harshit <harshit@Harshits-MacBook-Air.local>
2023-07-31 16:58:50 +0530

    pay user_id changes

--- js/order_screen.js
+++ js/order_screen.js
@@ -8,12 +8,14 @@ console.log('checking');
 getDATA();
 function getDATA(){
   const user_id = localStorage.getItem("user_id");
+  const google_user_id = localStorage.getItem("google_user_id");
   const gender = localStorage.getItem("gender");
 
   console.log('in data');
 // Do something with the user_id
   console.log("User ID:", user_id);
   console.log("gender:", gender);
+  console.log("google_user_id:", google_user_id);
 
 
 
