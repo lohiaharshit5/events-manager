99a2cadc490e61ed9ce124d033e3a6238744ac02
Harshit <harshit@Harshits-MacBook-Air.local>
2023-07-31 12:59:07 +0530

    error messages handling payment_gateway7

--- js/order_screen.js
+++ js/order_screen.js
@@ -3,3 +3,13 @@ function loadingfunction(){
   preloader.style.display = 'none';
   console.log('loader worked');
 }
+console.log('checking');
+
+getDATA();
+function getDATA(){
+  const user_id = localStorage.getItem("user_id");
+  console.log('in data');
+// Do something with the user_id
+  console.log("User ID:", user_id);
+
+}
